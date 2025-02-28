0304b08a1d5273963a6ef34eeae84bcc96038fc9
Alwinyuho <alwinyuho432@gmail.com>
2025-02-28 11:20:58 +0530

    app.py

--- app.py
+++ app.py
@@ -1,24 +1,53 @@
-from flask import Flask, render_template
-import os
-import psycopg2
+from flask import Flask, render_template, redirect, url_for, request
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your_secret_key'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'  # Database file
+db = SQLAlchemy(app)
 
-DATABASE_URL = os.environ['DATABASE_URL']
+# User Authentication Setup
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
 
-def get_db_connection():
-    conn = psycopg2.connect(DATABASE_URL, sslmode='require')
-    return conn
+# Define User Model
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(150), nullable=False)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 @app.route('/')
-def index():
-    conn = get_db_connection()
-    cur = conn.cursor()
-    cur.execute('SELECT * FROM my_table;')
-    results = cur.fetchall()
-    cur.close()
-    conn.close()
-    return render_template('index.html', results=results)
+def home():
+    return render_template('index.html')
 
-if _name_ == '_main_':
-    app.run(debug=True, host='0.0.0.0')
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username, password=password).first()
+        if user:
+            login_user(user)
+            return redirect(url_for('dashboard'))
+    return render_template('login.html')
+
+@app.route('/dashboard')
+@login_required
+def dashboard():
+    return f"Welcome {current_user.username}!"
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+if __name__ == "__main__":
+    db.create_all()  # Creates the database
+    app.run(debug=True)
