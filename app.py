6ccfd448e69f9b7e8bc7117f4f6bd6cb93fa0d67
Alwinyuho <alwinyuho432@gmail.com>
2025-02-28 10:52:30 +0530

    app.py

--- app.py
+++ app.py
@@ -1,81 +1,24 @@
-from flask import Flask, render_template, request, redirect, url_for, flash, session
-from flask_sqlalchemy import SQLAlchemy
-from flask_bcrypt import Bcrypt
-from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from flask import Flask, render_template
+import os
+import psycopg2
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'your_secret_key'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app = Flask(_name_)
 
-db = SQLAlchemy(app)
-bcrypt = Bcrypt(app)
-login_manager = LoginManager(app)
-login_manager.login_view = 'login'
+DATABASE_URL = os.environ['DATABASE_URL']
 
-# User Model
-class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(150), unique=True, nullable=False)
-    password = db.Column(db.String(150), nullable=False)
+def get_db_connection():
+    conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+    return conn
 
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
+@app.route('/')
+def index():
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM my_table;')
+    results = cur.fetchall()
+    cur.close()
+    conn.close()
+    return render_template('index.html', results=results)
 
-@app.route("/")
-def home():
-    return render_template("index.html")
-
-@app.route("/register", methods=["GET", "POST"])
-def register():
-    if request.method == "POST":
-        username = request.form["username"]
-        password = request.form["password"]
-        hashed_password = bcrypt.generate_password_hash(password).decode("utf-8")
-        
-        user_exists = User.query.filter_by(username=username).first()
-        if user_exists:
-            flash("Username already taken. Try another.", "danger")
-            return redirect(url_for("register"))
-
-        new_user = User(username=username, password=hashed_password)
-        db.session.add(new_user)
-        db.session.commit()
-        flash("Account created! You can now log in.", "success")
-        return redirect(url_for("login"))
-
-    return render_template("register.html")
-
-@app.route("/login", methods=["GET", "POST"])
-def login():
-    if request.method == "POST":
-        username = request.form["username"]
-        password = request.form["password"]
-        user = User.query.filter_by(username=username).first()
-
-        if user and bcrypt.check_password_hash(user.password, password):
-            login_user(user)
-            flash("Login successful!", "success")
-            return redirect(url_for("dashboard"))
-        else:
-            flash("Invalid username or password", "danger")
-
-    return render_template("login.html")
-
-@app.route("/dashboard")
-@login_required
-def dashboard():
-    return f"Welcome, {current_user.username}! <br><a href='/logout'>Logout</a>"
-
-@app.route("/logout")
-@login_required
-def logout():
-    logout_user()
-    flash("Logged out successfully!", "info")
-    return redirect(url_for("login"))
-
-if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()  # Create database tables
-    app.run(debug=True)
+if _name_ == '_main_':
+    app.run(debug=True, host='0.0.0.0')
